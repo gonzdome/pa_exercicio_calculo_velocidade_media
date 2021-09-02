b0dd38e68051004ba7ad3f748c921cfa4a658ed7
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-09-02 13:30:53 -0300

    Add project files.

---
+++ pa_exercicio_calculo_velocidade_media/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pa_exercicio_calculo_velocidade_media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double km, tempo;
+
+            Console.WriteLine("Qual tempo de viagem em horas?: ");
+            tempo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Qual distância: ");
+            km = double.Parse(Console.ReadLine());
+
+
+            km = km / tempo;
+
+            Console.WriteLine("A velocidade média é de " + Math.Round(km, 2) + " km/h!");
+
+            Console.ReadKey();
+        }
+    }
+}
